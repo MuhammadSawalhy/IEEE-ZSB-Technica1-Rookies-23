36ee24fe84893c33ef68e3f5e62dc53b2f329255
محمد الصوالحي <ms.2052001@gmail.com>
2022-12-06 15:40:24 +0200

    Fix problem 4 of the 5th task again

--- Task-5/4.py
+++ Task-5/4.py
@@ -1,7 +1,10 @@
 import math
 
 
-def prime_factors(n):
+def prime_factors(n: int):
+    if n < 2:
+        return []
+
     primes = []
 
     while n % 2 == 0:
@@ -10,11 +13,13 @@ def prime_factors(n):
 
     # + 2 to fix any precision errors
     for i in range(3, int(math.sqrt(n)) + 2, 2):
+        if i > n:
+            break
         while n % i == 0:
             primes.append(i)
             n //= i
 
-    if n != 1:
+    if n ^ 1:  # just fazlaka (same as --> n != 1)
         primes.append(n)
 
     return primes
